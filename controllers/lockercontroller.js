510b0eb92d36801034179b852ef65a7afaf32723
justisGipson <justis710@gmail.com>
2019-01-25 10:23:28 -0500

    stuff:

--- controllers/lockercontroller.js
+++ controllers/lockercontroller.js
@@ -21,7 +21,7 @@ router.post('/newItem', validateSesh, (req, res) => {
 });
 
 // get all gear for specific user
-router.get('/all/:id', validateSesh, (req, res) => {
+router.get('/all', validateSesh, (req, res) => {
     Locker.findAll({where: {owner: req.user.id}})
         .then(
             searchSuccess = (data) => {
